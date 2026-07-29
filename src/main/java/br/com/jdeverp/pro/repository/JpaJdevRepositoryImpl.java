928ef8a7d7550aac5f56d5523330c55e98479aeb
vic-barros <victoria2425laissa@gmail.com>
2026-07-28 21:05:01 -0300

    Adiciona dependência do Iterable no POM para auxiliar nos métodos genéricos do JpaJdevRepositoryImpl

--- src/main/java/br/com/jdeverp/pro/repository/JpaJdevRepositoryImpl.java
+++ src/main/java/br/com/jdeverp/pro/repository/JpaJdevRepositoryImpl.java
@@ -2,8 +2,11 @@ package br.com.jdeverp.pro.repository;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.commons.collections4.IterableUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +15,7 @@ import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
 public class JpaJdevRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID>
@@ -19,11 +23,13 @@ public class JpaJdevRepositoryImpl<T, ID extends Serializable> extends SimpleJpa
 
 	private final Class<T> domainClass; /* Classe model */
 	private final EntityManager entityManager; /* é o núcleo da persistência do JPA */
+	private final boolean multiEmpresa; /* Para não ficar chamando em todos os métodos */
 
 	public JpaJdevRepositoryImpl(Class<T> domainClass, EntityManager entityManager) {
 		super(domainClass, entityManager);
 		this.domainClass = domainClass;
 		this.entityManager = entityManager;
+		multiEmpresa = possuiEmpresa(); // Já chamou o método no construtor
 
 	}
 
@@ -31,16 +37,15 @@ public class JpaJdevRepositoryImpl<T, ID extends Serializable> extends SimpleJpa
 		super(entityInformation, entityManager);
 		this.domainClass = entityInformation.getJavaType();
 		this.entityManager = entityManager;
+		multiEmpresa = possuiEmpresa();
 	}
 
 	@Override
 	public Page<T> listarPaginado(Long empresaId, Pageable pageable) {
-		String entidade = domainClass.getSimpleName();
-		boolean possuiEmpresa = possuiEmpresa();
 
-		String jpql = "from " + entidade;
+		String jpql = "from " + domainClass.getSimpleName();
 
-		if (possuiEmpresa) {
+		if (multiEmpresa) {
 			jpql += " where empresa.id = :empresaId";
 		}
 
@@ -58,7 +63,7 @@ public class JpaJdevRepositoryImpl<T, ID extends Serializable> extends SimpleJpa
 
 		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
 
-		if (possuiEmpresa) {
+		if (multiEmpresa) {
 			query.setParameter("empresaId", empresaId);
 		}
 
@@ -71,22 +76,96 @@ public class JpaJdevRepositoryImpl<T, ID extends Serializable> extends SimpleJpa
 	// Faz a contagem de quantos registros de acordo com a empresa
 	@Override
 	public long total(Long empresaId) {
-		String entidade = domainClass.getSimpleName();
-		boolean possuiEmpresa = possuiEmpresa();
 
-		String jpql = "select count(*) from " + entidade;
-
-		if (possuiEmpresa) {
+		String jpql = "select count(*) from " + domainClass.getSimpleName();
+		if (multiEmpresa) {
 			jpql += " where empresa.id = :empresaId";
 		}
 
 		TypedQuery<Long> query = entityManager.createQuery(jpql, Long.class);
 
-		if (possuiEmpresa) {
+		if (multiEmpresa) {
 			query.setParameter("empresaId", empresaId);
 		}
 
 		return query.getSingleResult();
+	}
+
+	@Override
+	public Optional<T> buscarPorId(ID id, Long empresaId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<T> listar(Long empresaId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(ID id, long empresaId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	// Vamos precisar de uma dependência, para validar se esses Ids não são vazios
+	@Override
+	public List<T> buscarPorIds(Iterable<ID> ids, Long empresaId) {
+		if (IterableUtils.isEmpty(ids)) {
+			return Collections.emptyList();
+		}
+
+		String jpql = "from " + domainClass.getSimpleName() + " where id in :ids";
+
+		if (multiEmpresa) {
+			jpql += " and empresa.id = :empresaId";
+		}
+
+		TypedQuery<T> query = entityManager.createQuery(jpql, domainClass);
+		query.setParameter("ids", ids);
+
+		if (multiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+
+		return query.getResultList();
+	}
+
+	// Deletar todos por ID
+	@Override
+	public void deletarAllById(Iterable<ID> ids, Long empresaId) {
+		String jpql = "delete from " + domainClass.getSimpleName() + " where id in :ids";
+
+		if (multiEmpresa) {
+			jpql += " and empresa.id = :empresaId";
+		}
+
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("ids", ids);
+
+		if (multiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		query.executeUpdate();
+
+	}
+
+	// Deletar todos de acordo com o ID da Empresa
+	@Override
+	public long deleteAll(Long empresaId) {
+		String jpql = "delete from " + domainClass.getSimpleName();
+
+		if (multiEmpresa) {
+			jpql += " where empresa.id = :empresaId";
+		}
+
+		Query query = entityManager.createQuery(jpql);
+
+		if (multiEmpresa) {
+			query.setParameter("empresaId", empresaId);
+		}
+		return query.executeUpdate();
 	}
 
 	private boolean possuiEmpresa() {
