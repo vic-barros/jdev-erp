4ce1a8b599d24bf57c5d42ba67c8007d41af76bd
vic-barros <victoria2425laissa@gmail.com>
2026-07-28 21:05:01 -0300

    Adiciona dependência do Iterable no POM para auxiliar nos métodos genéricos do JpaJdevRepositoryImpl

--- src/main/java/br/com/jdeverp/pro/repository/JpaJdevRepository.java
+++ src/main/java/br/com/jdeverp/pro/repository/JpaJdevRepository.java
@@ -1,5 +1,8 @@
 package br.com.jdeverp.pro.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +14,26 @@ public interface JpaJdevRepository<T, ID> extends JpaRepository<T, ID> {
 	//Listar Paginado
 	Page<T>listarPaginado(Long empresaId, Pageable pageable);
 	
+	//Mostrar Total por Empresa
 	long total(Long empresaId);
+	
+	//Buscar por ID
+	Optional<T> buscarPorId(ID id, Long empresaId);
+	
+	//Listar tabela de acordo com a empresa
+	List<T> listar(Long empresaId);
+	
+	//Se existe cadastro pelo ID
+	boolean existsById(ID id, long empresaId);
+	
+	//Consulta cadastro pelo ID no banco
+	List<T> buscarPorIds(Iterable<ID> ids, Long empresaId);
+	
+	//Deletar vários elementos por Id
+	void deletarAllById(Iterable<ID> ids, Long empresaId);
+	
+	//Deletar tabela por inteira - apenas o registro de uma determinada empresa
+	long deleteAll(Long empresaId);
+	
 
 }
