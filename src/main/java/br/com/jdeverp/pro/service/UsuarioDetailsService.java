bd2d14d8723dc26ad52ed20187b73ad4d68c43fd
vic-barros <victoria2425laissa@gmail.com>
2026-08-17 12:23:00 -0300

    Adiciona classe da service: UserDetailsService, serve para fazer a busca por login quando o usuário estiver tentando acessar o sistema

---
+++ src/main/java/br/com/jdeverp/pro/service/UsuarioDetailsService.java
@@ -0,0 +1,28 @@
+package br.com.jdeverp.pro.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.jdeverp.pro.model.Usuario;
+import br.com.jdeverp.pro.repository.UsuarioRepository;
+
+@Service
+public class UsuarioDetailsService implements UserDetailsService {
+	// Carrega o usuário pelo seu Login, pega a Interface do UsuarioRepository e faz
+	// a injeção de dependênci apra usá-la (Autowired)
+
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.buscaPorLogin(username);
+
+		if (usuario != null) {
+			return usuario;
+		}
+		throw new UsernameNotFoundException(username + ": " + "Usuário não encontrado no banco de dados.");
+	}
+
+}
